41aa3fc04b3db80ad21152ab32b39a56a7d19dd9
Disha Rathod <drat0009@student.monash.edu>
2024-03-20 00:02:52 +1100

    Corrected description and made changes to roxygen skeleton of main function

--- bowlingpin/R/bowlingpin.R
+++ bowlingpin/R/bowlingpin.R
@@ -1,23 +1,63 @@
 #' Main function to run the game for two players
 #'
+#' @param player1_rolls A numeric vector having scores for every throw of player 1
+#' @param player2_rolls A numeric vector having scores for every throw of player 2
+#'
 #' @return A numeric vector containing the total score for each user.
 #' @export
 #'
 #' @examples
-#' bowlingpin()
-bowlingpin <- function() {
+#' player1_rolls <- c(2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2)
+#' player2_rolls <- c(2,2,2,2,8,2,6,2,2,2,2,2,5,2,2,2,4,2,2,3)
+#' bowlingpin(player1_rolls,player2_rolls)
+bowlingpin <- function(player1_rolls,player2_rolls) {
   cat("Welcome to the ten-pin bowling game!\n")
 
   # Get throws for Player 1
- player1_rolls <- rollinput(1)
   if (is.null(player1_rolls)) {
     return()
   }
 
   # Get throws for Player 2
-  player2_rolls <- rollinput(2)
   if (is.null(player2_rolls)) {
     return()
+  }
+
+  calculate_bowling_score <- function(throws) {
+    score <- 0
+    throw_index <- 1
+    frame <- 1
+
+    while (frame <= 10) {
+      if (frame == 10) {
+        # Check for the 10th frame
+        if (throws[throw_index] == 10) {  # Strike on first throw
+          score <- score + 10 + throws[throw_index + 1] + throws[throw_index + 2]
+          throw_index <- throw_index + 1
+        } else if (sum(throws[throw_index:(throw_index + 1)]) == 10) {  # Spare on first two throws
+          score <- score + 10 + throws[throw_index + 2]
+          throw_index <- throw_index + 2
+        } else {  # Open frame
+          score <- score + throws[throw_index] + throws[throw_index + 1]
+          throw_index <- throw_index + 2
+        }
+      } else {
+        if (throws[throw_index] == 10) {  # Strike
+          score <- score + 10 + throws[throw_index + 1] + throws[throw_index + 2]
+          throw_index <- throw_index + 1
+        } else if (sum(throws[throw_index:(throw_index + 1)]) == 10) {  # Spare
+          score <- score + 10 + throws[throw_index + 2]
+          throw_index <- throw_index + 2
+        } else {  # Open frame
+          score <- score + throws[throw_index] + throws[throw_index + 1]
+          throw_index <- throw_index + 2
+        }
+      }
+
+      frame <- frame + 1
+    }
+
+    return(score)
   }
 
   # Calculate scores for Player 1 and Player 2
