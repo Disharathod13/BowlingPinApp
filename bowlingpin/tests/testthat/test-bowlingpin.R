f7e9b778f3a4d8e05e0fc19ead5bc68eb6dc6678
Disha Rathod <drat0009@student.monash.edu>
2024-03-23 02:26:03 +1100

    deleting the testthat file for user input function

--- bowlingpin/tests/testthat/test-bowlingpin.R
+++ bowlingpin/tests/testthat/test-bowlingpin.R
@@ -1,4 +1,3 @@
-# Test case 1: depicts a situation where both players achieve perfect games, with each frame resulting in a strike.
 test_that("both players have perfect games", {
   player1_rolls <- c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
   player2_rolls <- c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
@@ -6,7 +5,7 @@ test_that("both players have perfect games", {
   expect_equal(bowlingpin(player1_rolls, player2_rolls), expected_output)
 })
 
-# Test case 2: involves a scenario where both players score zero in every frame,  only gutter balls for all frames.
+
 test_that("both players have all gutter balls", {
   player1_rolls <- rep(0, 20)
   player2_rolls <- rep(0, 20)
