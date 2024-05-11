09158c1b3b87c92b16c22a84034d65499768eb30
Disha Rathod <drat0009@student.monash.edu>
2024-05-11 14:25:39 +1000

    Need to fix the function and example in document modified.

--- R/winner.R
+++ R/winner.R
@@ -8,38 +8,24 @@
 #' @return A character string indicating the winner of the game ("Player 1", "Player 2", or "Tie").
 #'
 #' @examples
-#' player1_rolls <- c(2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2)
-#' player2_rolls <- c(2,2,2,2,8,2,6,2,2,2,2,2,5,2,2,2,4,2,2,3)
-#' bowling_leaderboard(player1_rolls, player2_rolls)
+#'
+#' player_rolls <- player_throws(player_no = c(1, 2, 3),
+#' throws = list(
+#' c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 6, 10),
+#' c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 3),
+#' c(1, 2, 3, 4, 5, 3, 1, 8, 9, 1, 2, 3, 4, 5, 2, 7, 8, 1, 10, 0, 10)
+#' ))
+#' bowling_leaderboard(player_rolls)
 #'
 #' @export
-bowling_leaderboard <- function(player1_rolls, player2_rolls) {
-  if (is.null(player1_rolls) || is.null(player2_rolls)) {
-    stop("Both players must have rolls.")
-  }
+bowling_leaderboard <- function(player_rolls) {
 
 # Calculate scores for Player 1 and Player 2
-  score_player1 <- calculate_bowling_score(player1_rolls)
-  score_player2 <- calculate_bowling_score(player2_rolls)
+  scores <- calculate_bowling_score(player_rolls)
 
 # Display scores
-  cat("Final scores:\n")
-  cat("Player 1:", score_player1, "\n")
-  cat("Player 2:", score_player2, "\n")
+  scores$scores
 
-# Determine the winner
-  if (score_player1 > score_player2) {
-    cat("Player 1 wins!")
-    winner <- "Player 1"
-  } else if (score_player2 > score_player1) {
-    cat("Player 2 wins!")
-    winner <- "Player 2"
-  } else {
-    cat("It's a tie!")
-    winner <- "Tie"
-  }
-
-  return(winner)
 }
 
 
