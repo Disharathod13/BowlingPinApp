78ba62a3c4fb6d9fca1de93ad9feea0294553657
Disha Rathod <drat0009@student.monash.edu>
2024-05-11 15:25:55 +1000

    Modified the function and updated the tests accordingly.

--- R/bowlingpin.R
+++ R/bowlingpin.R
@@ -1,31 +1,38 @@
-#' Main function to run the ten-pin bowling game for two players
-#' @description
-#' Enter your individual game scores and the function will give you the total score at the end
+#' Calculate Bowling Pin Scores
 #'
-#' @param player1_rolls A numeric vector having scores for every throw of player 1
-#' @param player2_rolls A numeric vector having scores for every throw of player 2
+#' This function calculates the total scores for each player's throws in a bowling game and prints it in custom vector format.
 #'
-#' @return A numeric vector containing the total score for each user.
+#'
+#' @param player_rolls A record containing player numbers and their throws.
+#' @return A record with players' scores calculated.
 #' @export
 #'
 #' @examples
-#' player_rolls<- player_throws(player_no = c(1, 2, 3),
-#' throws = list(
-#' c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 6, 10),
-#' c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 3),
-#' c(1, 2, 3, 4, 5, 3, 1, 8, 9, 1, 2, 3, 4, 5, 2, 7, 8, 1, 10, 0, 10)
-#' ))
+#' player_rolls <- player_throws(player_no = c(1, 2, 3),
+#'   throws = list(
+#'   c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 6, 10),
+#'   c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 3),
+#'   c(1, 2, 3, 4, 5, 3, 1, 8, 9, 1, 2, 3, 4, 5, 2, 7, 8, 1, 10, 0, 10)
+#'    ))
+#'
 #' bowlingpin(player_rolls)
-
+#'
 bowlingpin <- function(player_rolls) {
-  players_df <- vec_cast.data.frame.throws(player_rolls)
-
-  # Calculate sum of throws for each row and create a new column named "score"
-  players_df$score <- sapply(players_df$throws, sum)
+  players_df <- calculate_bowling_score(player_rolls)
 
   vec_cast.throws.data.frame((players_df))
 }
 
+#' Calculate the sum of throws
+#'
+#' This function calculates the total scores for each player's throws in a bowling game and returns a data frame.
+#'
+#'
+#' @param player_rolls A record containing player numbers and their throws.
+#' @return A record with players' scores calculated.
+#'
+#'@keywords internal
+#'
 calculate_bowling_score <- function(throws) {
   players_df <- vec_cast.data.frame.throws(throws)
 
--- tests/testthat/test-bowlingpin.R
+++ tests/testthat/test-bowlingpin.R
@@ -1,16 +1,19 @@
-test_that("both players have perfect games", {
-  player1_rolls <- c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-  player2_rolls <- c(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-  expected_output <- c(300, 300)
-  expect_equal(bowlingpin(player1_rolls, player2_rolls), expected_output)
+test_that("bowlingpin function calculates scores correctly", {
+  player_rolls <- player_throws(player_no = c(1, 2, 3),
+                                throws = list(
+                                  c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 6, 10),
+                                  c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 3),
+                                  c(1, 2, 3, 4, 5, 3, 1, 8, 9, 1, 2, 3, 4, 5, 2, 7, 8, 1, 10, 0, 10)
+                                ))
+  result <- bowlingpin(player_rolls)
+  expected_output <- vctrs::new_rcrd(list(
+    player_no = c(1, 2, 3),
+    throws = list(
+      c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 6, 10),
+      c(2, 3, 4, 5, 6, 4, 8, 1, 1, 2, 3, 4, 5, 5, 7, 2, 9, 1, 4, 3),
+      c(1, 2, 3, 4, 5, 3, 1, 8, 9, 1, 2, 3, 4, 5, 2, 7, 8, 1, 10, 0, 10)
+    ),
+    score = c(92, 79, 89)
+  ), class = "throws")
+  expect_equal(result, expected_output)
 })
-
-
-test_that("both players have all gutter balls", {
-  player1_rolls <- rep(0, 20)
-  player2_rolls <- rep(0, 20)
-  expected_output <- c(0, 0)
-  actual_output <- bowlingpin(player1_rolls, player2_rolls)
-  expect_equal(actual_output, expected_output)
-})
-
